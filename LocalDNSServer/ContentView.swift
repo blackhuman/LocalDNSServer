d614f92ddc0495f1fc5d32d2c6eb1d2e467d743f
白一哲 <baiyizhe@meituan.com>
2021-08-10 13:41:24 +0800

    Initial Commit

---
+++ LocalDNSServer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalDNSServer
+//
+//  Created by 白一哲 on 2021/8/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocalDNSServer/LocalDNSServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalDNSServerApp.swift
+//  LocalDNSServer
+//
+//  Created by 白一哲 on 2021/8/10.
+//
+
+import SwiftUI
+
+@main
+struct LocalDNSServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
